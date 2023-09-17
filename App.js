0d7ff7e28fe89f150bd1bf63687f7dd49cd12fad
VISHNU M K <mail.vishnumk@gmail.com>
2023-09-17 17:21:34 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { Text, TextInput, TouchableHighlight, View } from 'react-native';
 import { Styles } from './Style/Style';
 import { useState } from 'react';
+import {FontAwesome} from "@expo/vector-icons"
 
 export default App = () => {
 
@@ -10,25 +11,72 @@ export default App = () => {
 
     const handleSubmit = () => {
         if(toDo.trim() === ""){
-          alert("Enter a valid item")
-        }
-        setToDos([...toDos,{item:toDo,id:new Date().getTime()}])
+            showError("Enter A Valid Task!")
+            setTimeout(() => {
+                showError("")
+            }, 2000);
+        }else{
+            const findIndex = toDos.find(item => item.item.toLowerCase() === toDo.trim().toLowerCase())
+            if(findIndex){
+                showError("Task Already Exist!")
+                setTimeout(() => {
+                    showError("")
+                }, 2000);
+            }else{
+                setToDos([...toDos,{item:toDo,createdAt:new Date().getTime(),updatedAt:"",checked:false}])
+                setToDo("")
+            }
+        } 
+    }
+
+    const checkBox = (item) => {
+        toDos.splice(toDos.indexOf(item),1)
+        setToDos([...toDos, {...item,updatedAt:new Date().getTime(),checked:!item.checked}])
+    }
+
+    const removeData = (item) => {
+        toDos.splice(toDos.indexOf(item),1)
+        setToDos([...toDos])
     }
 
     return (
         <View style={Styles.container}> 
-            <TextInput style={Styles.toDoInput} placeholder='Enter title...' onChange={(e)=>setToDo(e.nativeEvent.text)} placeholderTextColor="gray" value={toDo}></TextInput>
+            <TextInput style={Styles.toDoInput} placeholder='Enter Task Name...' onChange={(e)=>setToDo(e.nativeEvent.text)} placeholderTextColor="gray" value={toDo}></TextInput>
+           {error && <Text style={Styles.errorText}>{error}</Text>}
             <TouchableHighlight style={Styles.addItem} underlayColor="#fff" onPress={handleSubmit}>
                 <Text style={Styles.addItemText}>Add Item</Text>
             </TouchableHighlight>
-
             {
-                toDos.sort((a,b)=>b.id - a.id).map(item => {
-                    return (
-                        <Text style={Styles.listItem} key={item.id}>{item.item}</Text>
-                    )
-                })
+                toDos.length===0 && 
+                    <Text style={{marginHorizontal:20,color:"#F15A59"}}>Note: {"\n\n"}1. Click on the text, it will mark as completed or back to pending list!{"\n\n"}2. Click on the close icon, it will remove the task from the list</Text>
+                
             }
+            {toDos.filter(item=>item.checked===false).length !==0 && <Text style={Styles.pendingTask}>Pending Tasks ( {toDos.filter(item=>item.checked===false).length} )</Text>}
+                {
+                    toDos.sort((a,b)=>b.createdAt - a.createdAt).filter(items=>items.checked === false).map(item => {
+                        return (
+                            <View style={Styles.listItem} key={item.createdAt} >
+                                <Text onPress={()=>checkBox(item)}>
+                                    <Text style={{fontFamily:"serif",color:"white"}}>{item.item}{"\n"}{new Date(item.createdAt).toLocaleString("default",{day:"2-digit",month:"2-digit",year:"numeric",hour:"2-digit",minute:"2-digit",second:"2-digit",hour12:true})}</Text>
+                                </Text>
+                                <FontAwesome name='close' style={Styles.closebtn} onPress={()=>removeData(item)}></FontAwesome>
+                            </View>
+                        )
+                    })
+                }
+
+                {toDos.filter(item=>item.checked===true).length !==0 && <Text style={Styles.pendingTask}>Completed Tasks ( {toDos.filter(item=>item.checked===true).length} )</Text>}
+                {
+                    toDos.sort((a,b)=>b.createdAt - a.createdAt).filter(items=>items.checked === true).map(item => {
+                        return (
+                          <View style={Styles.listItem2} key={item.createdAt} >
+                              <Text onPress={()=>checkBox(item)}>
+                                  <Text style={{fontFamily:"serif", color:"white"}}>{item.item}{"\n"}{new Date(item.updatedAt).toLocaleString("default",{day:"2-digit",month:"2-digit",year:"numeric",hour:"2-digit",minute:"2-digit",second:"2-digit",hour12:true})}</Text>
+                              </Text>
+                          </View>
+                        )
+                    })
+                }
 
         </View>
     )
--- Style/Style.js
+++ Style/Style.js
@@ -17,7 +17,7 @@ export const Styles = StyleSheet.create({
         borderRadius:15,
         color:"#fff"
     },
-    addItemText:{
+    addItemText:{ 
         fontFamily:"serif",
         color:"#ffffff"
     },
@@ -25,7 +25,7 @@ export const Styles = StyleSheet.create({
         backgroundColor:"#635985",
         paddingHorizontal:40,
         paddingVertical:8,
-        marginTop:16,
+        marginTop:10,
         borderRadius:12,
         shadowColor:"black",
         shadowOpacity:1,
@@ -34,11 +34,70 @@ export const Styles = StyleSheet.create({
             height:2
         },
         shadowRadius:5,
-        elevation:4
+        elevation:4,
+        marginBottom:10
     },
     listItem:{
-        borderWidth:1,
         borderRadius:15,
+        color:"#fff",
+        padding:15,
+        marginVertical:5,
+        width:350,
+        backgroundColor:"#F15A59",
+        color:"white"
+    },
+    listItem2:{
+        borderRadius:15,
+        color:"#fff",
+        padding:15,
+        marginVertical:5,
+        width:350,
+        backgroundColor:"#4E9F3D",
+        color:"white",
+    },
+    errorText:{
+        marginTop:3,
+        color:"#F15A59",
+        fontFamily:"monospace"
+    },
+    listView:{
+        marginTop:30
+    },
+    // checkboxNotChecked:{
+    //     width: 20,
+    //     height: 20,
+    //     borderWidth: 1,
+    //     borderRadius:5,
+    //     borderColor: 'white'
+    // },
+    // checkboxChecked:{
+    //     width: 20,
+    //     height: 20,
+    //     borderWidth: 1,
+    //     borderRadius:5,
+    //     borderColor: 'white',
+    //     backgroundColor:"#4E9F3D",
+    //     flex:1,
+    //     alignItems:"center",
+    //     color:"white"
+    // },
+    itemName:{
+       marginLeft:10,
+       maxWidth:350
+    },
+    pendingTask:{
+        fontSize:18,
+        color:"#fff",
+        marginTop:8
+    },
+    centerfy:{
+        padding:10
+    },
+    closebtn:{
+        position:"absolute",
+        right:3,
+        top:3,
+        fontSize:20,
         color:"#fff"
     }
 })
